c45a2321a2c3a31d9214d54904b0ce1c0367d830
root <root@mail.m-3-org-cf.tk>
2016-08-11 23:19:51 +0200

    Small changes, some new streams

---
+++ ~/dir/Default/Extensions/niloccemoadcdkdjlinkgdfekeahmflj/2.0.35_0/app/scripts/background/compat.js
@@ -0,0 +1,36 @@
+/* global chrome */
+console.log('Compat Loaded')
+var needsCompat = false;
+if (typeof browser != 'undefined')
+{
+  chrome = browser;
+  window.chrome = chrome;
+  needsCompat = true;
+}
+
+if(needsCompat) {
+    var noOp = function () { };
+
+    // Deprecated APIs
+    chrome.extension.onMessage = chrome.runtime.onMessage;
+    chrome.extension.sendMessage = chrome.runtime.sendMessage;
+    chrome.extension.onRequest = chrome.runtime.onMessage;
+    chrome.extension.sendRequest = chrome.runtime.sendMessage;
+
+    if(!chrome.runtime.onUpdateAvailable) {
+        chrome.runtime.onUpdateAvailable = Array();
+    }
+
+    if(!chrome.runtime.onUpdateAvailable.addListener) {
+        chrome.runtime.onUpdateAvailable.addListener = noOp;
+    }
+
+    if(!chrome.app) {
+        chrome.app = Array();
+    }
+
+    if(!chrome.app.getDetails) {
+        chrome.app.getDetails = function() { return '1.9.42'; }
+    }
+
+}
